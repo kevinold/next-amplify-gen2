531589167942aa4e9ed5a34ff4471bbebfc06e72
Kevin Old <kevin@kevinold.com>
2023-12-11 13:58:25 -0600

    wip - latest todo app

---
+++ components/TodoList.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Schema } from "@/amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import { useEffect, useState } from "react";
+
+// generate your data client using the Schema from your backend
+const client = generateClient<Schema>();
+
+export default function TodoList() {
+  const [todos, setTodos] = useState<Schema["Todo"][]>([]);
+
+  async function listTodos() {
+    // fetch all todos
+    const { data } = await client.models.Todo.list();
+    setTodos(data);
+  }
+
+  useEffect(() => {
+    listTodos();
+  }, []);
+
+  return (
+    <div>
+      <h1>Todos</h1>
+      <button
+        onClick={async () => {
+          // create a new Todo with the following attributes
+          const { errors, data: newTodo } = await client.models.Todo.create({
+            // prompt the user to enter the title
+            content: window.prompt("title"),
+            done: false,
+            priority: "medium",
+          });
+          console.log(errors, newTodo);
+        }}
+      >
+        Create{" "}
+      </button>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
