c169a10d1f331c331bd16355608b0338cd9b1bf5
Kevin Old <kevin@kevinold.com>
2023-12-13 16:03:27 -0600

    update TodoList to use be a server component with an action

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,50 +1,31 @@
-"use client";
-
 import { Schema } from "@/amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-import { useEffect, useState } from "react";
+import { revalidatePath } from "next/cache";
 
 // generate your data client using the Schema from your backend
 const client = generateClient<Schema>();
 
-export default function TodoList() {
-  const [todos, setTodos] = useState<Schema["Todo"][]>([]);
+export default async function TodoList() {
+  const { data: todos } = await client.models.Todo.list();
 
-  async function listTodos() {
-    // fetch all todos
-    const { data } = await client.models.Todo.list();
-    setTodos(data);
+  async function addTodo(data: FormData) {
+    "use server";
+    const title = data.get("title") as string;
+    const { errors, data: newTodo } = await client.models.Todo.create({
+      content: title,
+      done: false,
+      priority: "medium",
+    });
+    revalidatePath("/");
   }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  useEffect(() => {
-    const sub = client.models.Todo.observeQuery().subscribe(({ items }) =>
-      setTodos([...items])
-    );
-
-    return () => sub.unsubscribe();
-  }, []);
 
   return (
     <div>
       <h1>Todos</h1>
-      <button
-        onClick={async () => {
-          // create a new Todo with the following attributes
-          const { errors, data: newTodo } = await client.models.Todo.create({
-            // prompt the user to enter the title
-            content: window.prompt("title"),
-            done: false,
-            priority: "medium",
-          });
-          console.log(errors, newTodo);
-        }}
-      >
-        Create{" "}
-      </button>
+      <form action={addTodo}>
+        <input type="text" name="title" />
+        <button type="submit">Add Todo</button>
+      </form>
 
       <ul>
         {todos.map((todo) => (
