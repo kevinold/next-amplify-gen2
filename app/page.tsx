d3aaae90e39e301e65f57d89e94033364514396c
Kevin Old <kevin@kevinold.com>
2024-01-12 16:44:40 -0600

    refactor away from todo component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
-import TodoList from "@/components/TodoList";
+import { cookiesClient } from "@/utils/amplify-utils";
+import { revalidatePath } from "next/cache";
 
-function App() {
+async function App() {
+  const { data: todos } = await cookiesClient.models.Todo.list();
+
+  async function addTodo(data: FormData) {
+    "use server";
+    const title = data.get("title") as string;
+    await cookiesClient.models.Todo.create({
+      content: title,
+      done: false,
+      priority: "medium",
+    });
+    revalidatePath("/");
+  }
+
   return (
     <>
       <h1>Hello, Amplify 👋</h1>
-      <TodoList />
+      <form action={addTodo}>
+        <input type="text" name="title" />
+        <button type="submit">Add Todo</button>
+      </form>
+
+      <ul>
+        {todos && todos.map((todo) => <li key={todo.id}>{todo.content}</li>)}
+      </ul>
     </>
   );
 }
