30f19873fba1c5143d524f698c2abc47abedff9b
Kevin Old <kevin@kevinold.com>
2024-01-25 10:39:45 -0600

    add AuthUser type to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { AuthUser } from "@aws-amplify/auth";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
-function Login({ user }) {
+function Login({ user }: { user: AuthUser }) {
   useEffect(() => {
     if (user) {
       redirect("/");
